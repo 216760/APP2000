6a99301763c8465da1e6da6dde32c5a3a65b7d8a
Andreas <andreas_k83@hotmail.com>
2020-02-18 15:01:10 +0100

    Foreløpig opprydding i filer

---
+++ frontend/login.php
@@ -0,0 +1,31 @@
+ <div class="wrapper fadeInDown">
+
+  <div id="formContent">
+    <!-- Tabs Titles -->
+
+    <!-- Icon -->
+    <div class="fadeIn first">
+      <img src="mordi.png" id="icon" alt="User Icon" />
+    </div>
+
+    <!-- Login Form -->
+    <form action="includes/login.inc.php" method="post">
+      <input type="text" id="login" class="fadeIn second" name="login" placeholder="login" required>
+      <input type="text" id="password" class="fadeIn third" name="login" placeholder="password" required>
+      <input type="submit" class="fadeIn fourth" value="Log In">
+    </form>
+    
+
+
+    
+
+  </div>
+</div>
+
+
+<head>
+  <link rel="stylesheet" href="CSS/Stylesheet2.css">
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  </head>
