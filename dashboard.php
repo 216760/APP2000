20d2c2252725e9eba3f0aab6f88d3b82a6b14179
Andreas Knutsen <31688396+216760@users.noreply.github.com>
2020-03-31 13:00:02 +0200

    Add files via upload

--- dashboard.php
+++ dashboard.php
@@ -47,7 +47,7 @@ include('includes/navbar.php');
 // ----------------------------------------------------------------------------------------------------
 
 // $connection = mysqli_connect("itfag.usn.no", "v20app2000u2", "pw2", "v20app2000db2");
-$connection = mysqli_connect("localhost", "root", "", "gruppe2");
+$connection = mysqli_connect("itfag.usn.no", "v20app2000u2", "pw2", "v20app2000db2");
 $query = "SELECT * FROM cards";
 $query_run = mysqli_query($connection, $query);
 
@@ -62,109 +62,96 @@ $query_run = mysqli_query($connection, $query);
  Setter opp dashboard struktur. Vi henter så ut brukerkort fra databasen ved å identifisere dem via id 
 ----------------------------------------------------------------------------------------------------- -->
 
-<html>
+<div class="content-dashboard">
+  <div class="container">
+    <div class="row justify-content-center">
+        <?php
 
-<head>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<link rel="stylesheet" href="css/stylesheet.css">
-</head>
-<body>
-<h6 class="info-text">Please add new subscription in the top right</h6>
-                    <div class="container">
-                        <div class="row">
-                        <?php
-                            //Hvis det finnes rader/data i db
-                            if(mysqli_num_rows($query_run) > 0)
-                            {
-                            //og spørring returnerer data, blir dataene presenter i en tabell 
-                            while($row = mysqli_fetch_assoc($query_run)) {
-                                ?>
-                                        <!-- Tabellen blir vist frem ved hjelp av DOM elementer -->
-                                        <div class="col-sm-4">
-                                            <div class="card shadow mx-auto" style="width: 18rem;">
-                                            <ul class="list-group list-group-flush">  
-                                                <h5 class="list-group-item">Service <?php echo $row['description']; ?></h5>    
-                                                <p class="list-group-item">Start date <?php echo $row['start_date']; ?></p>
-                                                <p class=list-group-item>End date <?php echo $row['end_date']; ?></p>
-                                                </ul>
-                                                <div class="card-body">
-                                                <form action="edit.php" method="POST">                <!-- Henter ut id fra DB -->
-                                                    <input type="hidden" name ="edit_id" value="<?php echo $row['id']; ?>">
-                                                        <button type="submit" name="edit_btn" class="btn btn-primary"> Edit</button>
-                                                </form> 
-                                                <form action="server.php" method="POST">                <!-- Henter ut id fra DB -->
-                                                    <input type="hidden" name ="delete_id" value="<?php echo $row['id']; ?>">
-                                                    <button type="submit" name="delete_btn" class="btn btn-primary"> Delete</button>
-                                                    </form>                             
-                                            </div>
-                                        </div>
-                                    </div>
-                                <?php }?>
-                            </div>
-                        </div>
-                        <?php
-                        }
-                        else {
-                        echo '<h7 class="empty-sub">No subscriptions yet :(</h7>';
-                        }
-                    ?>
-                    <div class="container">
-                        <div class="modal" id="eexampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                            <h5 class="modal-title" id="addServiceLabel">Add service</h5>
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                            </div>
-                                            <form action="server.php" method="POST">
-                                                <div class="modal-body">
-                                                    <div class="form-group">
-                                                    <label> Service </label>
-                                                    <input type="text" name="description" value=""  class="form-control" >
-                                            </div>
-                                            <div class="form-group">
-                                                    <label>Trial from</label>
-                                                    <input type="date" name="start_date" value=""  class="form-control" >
-                                            </div>
-                                            <div class="form-group">
-                                                    <label>Trial to</label>
-                                                        <input type="date" name="end_date" value="" class="form-control">
-                                            </div>
-                                            <div class="modal-footer">
-                                            <button type="button" class="btn" data-dismiss="modal">Close</button>
-                                            <button type="submit" name="registerbtn" class="btn">Save</button>
-                                            </div>
+        if(mysqli_num_rows($query_run) > 0) {
 
-                                            <?php
+          while($row = mysqli_fetch_assoc($query_run)) {
 
-                                            if(isset($_SESSION['subOk']) && $_SESSION['subOk'] != '')
-                                            {
-                                            echo '<h2> ' .$_SESSION['subOk']. ' </h2>';
-                                            unset($_SESSION['subOk']);
-                                            }
+        ?>
+      <div class="col-sm-4">
+        <div class="card shadow mx-auto" style="width: 18rem;">
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item"><h6 class="card-text">Service</h6><?php echo $row['description']; ?></li>
+            <li class="list-group-item"><h6 class="card-text">Start date</h6><?php echo $row['start_date']; ?></li>
+            <li class="list-group-item"><h6 class="card-text">End date</h6><?php echo $row['end_date']; ?></li>
+          </ul>
+          <div class="card-body">
+            <form action="subscription_edit.php" method="post">                                               
+              <input type="hidden" name ="edit_id" value="<?php echo $row['id']; ?>">
+                  <button type="submit" class="btn btn-primary" name="edit_btn" data-toggle="modal"> Edit</button>
+                  <button type="button" class="btn btn-primary" data-toggle="modal"> Delete</button>
+            </form>    
+          </div>
+        </div>
+      </div>
+          <?php } ?>
+    </div>
+  </div>
+</div>
 
-                                            if(isset($_SESSION['subError']) && $_SESSION['subError'] != '')
-                                            {
-                                            //   echo '<h2 class="bg-info"> ' .$_SESSION['subError']. ' </h2>';
-                                            unset($_SESSION['subError']);
-                                            }
-                                            ?>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>                  
+  <?php
+    }
+    else { ?>
+      <h6 class="info-text">Please add new subscription in the top right</h6> <?php
+    }
+  ?>
+<div class="container">
+  <div class="modal" id="eexampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <!-- <h5 class="modal-title" id="exampleModalLabel">Legg til abonnoment</h5> -->
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+              
+        <form action="server.php" method="POST">
+          <div class="modal-body">
+            <div class="form-group">
+                <label> Tjeneste </label>
+                <input type="text" name="description" value=""  class="form-control" >
+            </div>
+            <div class="form-group">
+                <label>Dato fra</label>
+                <input type="date" name="start_date" value=""  class="form-control" >
+            </div>
+            <div class="form-group">
+                <label>Dato til</label>
+                <input type="date" name="end_date" value="" class="form-control">
+            </div>
+
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="submit" name="registerbtn" class="dashbtn btn-success">Save</button>
+            </div>
+
+          <?php
+
+          if(isset($_SESSION['success']) && $_SESSION['success'] != ''){
+            echo '<h2> ' .$_SESSION['success']. ' </h2>';
+            unset($_SESSION['success']);
+          }
+
+          if(isset($_SESSION['error']) && $_SESSION['error'] != '') {
+            echo '<h2 class="bg-info"> ' .$_SESSION['error']. ' </h2>';
+            unset($_SESSION['error']);
+          }
+          ?>
+        </div>
+      </form>
+      </div>
+    </div>
+  </div>
+</div>               
 
 <!-- ---------------------------------------------------------------------------------------------------- 
 
 ----------------------------------------------------------------------------------------------------- -->
-
-
-
-
 
 
 <!-- ---------------------------------------------------------------------------------------------------- 
@@ -179,3 +166,12 @@ Footer
 
 </body> 
 </html>
+<!-- ---------------------------------------------------------------------------------------------------- 
+Henter script filer (JS, JQUERY, BOOTSTRAP)
+----------------------------------------------------------------------------------------------------- -->
+
+<?php include'includes/scripts.php';?>
+
+<!-- ---------------------------------------------------------------------------------------------------- 
+Henter script filer (JS, JQUERY, BOOTSTRAP)
+----------------------------------------------------------------------------------------------------- -->
--- index.php
+++ index.php
@@ -1,34 +1,36 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Forside</title>
-	<link rel="stylesheet" href="bootstrap-4.1.3-dist/css/bootstrap.min.css">
-	<link rel="stylesheet" href="CSS/stylesheet.css">
-	<link rel="stylesheet" href="CSS/stylefixed.css">
-</head>
+<?php
+session_start();
+include('includes/header.php');
+include('includes/navbar.php'); 
+?>
 
-<body data-spy="scroll" data-target="#navbarResponsive">
-		<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-			<?php include('includes/navbar.php');?>
-		</nav>
 
-			<?php include('includes/main.php');?>
-			
-<!-- ---------------------------------------------------------------------------------------------------- 
-Henter script filer (JS, JQUERY, BOOTSTRAP)
------------------------------------------------------------------------------------------------------ -->
+<!--  Start Info Section -->
 
-<script src="js/jquery-3.3.1.min.js"></script>
-<script src="bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
-<script src="https://use.fontawesome.com/releases/v5.5.0/js/all.js"></script>
+	<div class="content-main">
+		<?php include'includes/main.php';?>
+	</div>
 
-<!-- ---------------------------------------------------------------------------------------------------- 
-Henter script filer (JS, JQUERY, BOOTSTRAP)
------------------------------------------------------------------------------------------------------ -->
+<!-- End Info Section -->
 
+
+<!--  Start Contact Section -->
+	<footer>
+		<?php include'includes/footer.php';?>
+	</footer>
+<!-- End Contact Section -->
 
 
 </body>
 </html>
+
+
+<!-- ---------------------------------------------------------------------------------------------------- 
+Henter script filer (JS, JQUERY, BOOTSTRAP)
+----------------------------------------------------------------------------------------------------- -->
+
+<?php include'includes/scripts.php';?>
+
+<!-- ---------------------------------------------------------------------------------------------------- 
+Henter script filer (JS, JQUERY, BOOTSTRAP)
+----------------------------------------------------------------------------------------------------- -->
