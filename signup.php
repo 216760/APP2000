48d15d1fb77fa20a6d25289bcab142c418469657
Andreas Knutsen <31688396+216760@users.noreply.github.com>
2020-04-02 12:40:38 +0200

    reupload

--- signup.php
+++ signup.php
@@ -1,60 +1,59 @@
+<?php
+session_start();
+?>
+
+
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta name="description" content="">
-		<meta name="author" content="">
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/stylesheet.css">
+        <link rel="stylesheet" href="css/login.css">
+    </head>
+    <body>
 
-		<title>Registrering</title>
+             
+        <form id="myForm" class="form-signin" form action="signup.php" method="post" name="form1">
+            <h1>LOGO</h1>
+            <input type="text" placeholder="username" class="" name="name">
+            <input type="text" placeholder="email" class="" name="email">
+            <input type="password" placeholder="password" class="" name="password"> 
+            <input type="submit" id="regbtn" name="registerbtn" class="btn rounded primary" value="Registrer">
+            <?php
 
-		<link rel="stylesheet" href="CSS/stylesheet.css">
-		<link rel="stylesheet" href="CSS/Logginn.css">
-	</head>
-	<body>					
-<form id="myForm" form action="signup.php" method="post" name="form1">
-	<div class="content">
-		<h1>LOGO</h1>
+                if(isset($_SESSION['status']) && $_SESSION['status'] !='') {
+                    echo '<h6 class="bg-warning text-white"> '.$_SESSION['status'].' </h6>';
+                    unset($_SESSION['status']);
+                } else if (isset($_SESSION['success']) && $_SESSION['success'] !='') {
+                    echo '<h6 class="bg-success text-white"> '.$_SESSION['success'].' </h6>';
+                    unset($_SESSION['success']);
+                }
 
-		<input type="text" placeholder="username" class="" name="name"><br>
-		<br>
-		<input type="text" placeholder="email" class="" name="email"><br>
-		<br>
-		<input type="password" placeholder="password" class="" name="password"> 
-		<br>
-		<input type="submit" id="regbtn" name="registerbtn" class="btn rounded primary" value="Registrer">
+            ?>
 
-	</div>
-</form>
+        </form>
 
 </body>
 </html>
-	
+    
 <!----------------------------------------------------------------------------------------------------------------------------------------------
-
 TIL INFORMASJON: 
-
 I denne filen finnes det kode som er gjenbrukt og tilpasset kode som er funnet på linkene oppsummert under. 
 Dette er også synlig kommentert andre steder i filen der hvor dette oppstår, og vil også bli dokumentert under kildebruk i rapporten.
-
 Grunnen til dette er fordi at for enkelte funksjoner så er det “best practice”-måter å kode på. For vår del så er det en måte å lære dette på. 
 Vi setter oss inn i all koden først, og sørger for at vi forstår den, før den eventuelt vil bli tilpasset og implementert i vår kode.
-
 Kilde: 
 https://www.youtube.com/playlist?list=PLRheCL1cXHrvTkUenAc5GdEvqIpVX-2JJ
-
-
-
 ------------------------------------------------------------------------------------------------------------------------------------------------> 
 
-
         <?php
-        session_start();
         //Inkluderer database connection-fil
         include_once("db-config.php");
-
 
         $empty = FALSE;
 
@@ -62,38 +61,31 @@ https://www.youtube.com/playlist?list=PLRheCL1cXHrvTkUenAc5GdEvqIpVX-2JJ
         if (isset($_POST['registerbtn'])) {
             $name     = $_POST['name'];
             $email    = $_POST['email'];
-			$password = $_POST['password'];
+            $password = $_POST['password'];
 
-
-			// $password= password_hash($_POST["password"],PASSWORD_DEFAULT);
+            // $password= password_hash($_POST["password"],PASSWORD_DEFAULT);
             $empty = FALSE;
             $emailErr = "";
+            
+            if(!empty($_POST['name'] && $_POST['email'] && $_POST['password'])) {
 
-        	if(!empty($_POST['name'] && $_POST['email'] && $_POST['password'])) {
+                // Hvis email allerede er registert får bruker en feilmelding
+                $email_result = mysqli_query($mysqli, "select 'email' from register where email='$email' and password='$password'");
 
-	            // Hvis email allerede er registert får bruker en feilmelding
-	            $email_result = mysqli_query($mysqli, "select 'email' from register where email='$email' and password='$password'");
+                // Teller antall rader som matcher med email som bruker skrev i registreringsformen
+                $user_matched = mysqli_num_rows($email_result);
 
-	            // Teller antall rader som matcher med email som bruker skrev i registreringsformen
-	            $user_matched = mysqli_num_rows($email_result);
+                // Gjør email sikker på SQL injekting (saniterer)
+                $email = filter_var($email, FILTER_SANITIZE_EMAIL);
 
-	            // Gjør email sikker på SQL injekting (saniterer)
-				$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+                // Om emailen ikke er i riktig regex format blir den ikke sanitert
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 
-				// Om emailen ikke er i riktig regex format blir den ikke sanitert
-	            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    $_SESSION['status'] = "$email is not a valid email address";
+                    header('Location: signup.php');
 
-            		echo("$email is not a valid email address");
+                } else {
 
-            	} else {
-
-            		echo"";
-					// Hvis spørringen returnerer et antall bruker-rader som er større enn 0, betyr det at emailen allerede eksisterer i databasen
-            		if ($user_matched > 0) {
-
-            			echo "<br/><br/><strong>Error: </strong> User already exists with the email id '$email'.";
-
-            		} else {
 
 // ----------------------------------------------------------------------------------------------------
 // Operasjon med å hashe brukerregistrert passord er hentet og tilpasset egen løsning fra: 
@@ -101,42 +93,29 @@ https://www.youtube.com/playlist?list=PLRheCL1cXHrvTkUenAc5GdEvqIpVX-2JJ
 // https://www.youtube.com/watch?v=3bGDe0rbImY&t=635s
 // ----------------------------------------------------------------------------------------------------
 
-						// Variabel som bruker password_hash metode for å hashe passord
-						$hashedpass = password_hash($password, PASSWORD_BCRYPT);
-						
-						// Setter brukerdata inn i databasen
-            			$result = mysqli_query($mysqli, "INSERT INTO register(username,email,password) VALUES('$name','$email','$hashedpass')");
-	
-						// Ved gjennomført spørring får brukeren beskjed om at profilen er opprettet eller at det har skjedd en feil. 
-            			if ($result) {
+                    // Variabel som bruker password_hash metode for å hashe passord
+                    $hashedpass = password_hash($password, PASSWORD_BCRYPT);
+                    
+                    // Setter brukerdata inn i databasen
+                    $result = mysqli_query($mysqli, "INSERT INTO register(username,email,password) VALUES('$name','$email','$hashedpass')");
+    
+                        // Ved gjennomført spørring får brukeren beskjed om at profilen er opprettet eller at det har skjedd en feil. 
+                        if ($result) {
 
-            				echo "<br/><br/>User Registered successfully.";
+                            $_SESSION['success'] = "$User Registered successfully";
+                            header('Location: signup.php');
 
-            			} else {
 
-            				echo "Registration error. Please try again." . mysqli_error($mysqli);
+                        } else {
+                            // Todo: Finn en bedre måte å vise denne meldingen på
+                            $_SESSION['status'] = "Email already in use: \n" . mysqli_error($mysqli);
 
-            			}
-            		}
-            	}
+                        }
+                }
+            } else {
+                $_SESSION['status'] = "Fields cannot be empty";
+                header('Location: signup.php');
             }
         }
-
-        ?>
-
-
-<!-- 
-<script type="text/javascript">
-    function Validate() {
-        var password = document.getElementById("password").value
-        var confirmPassword = document.getElementById("confirmpass").value
-        if(password != confirmPassword) {
-            alert('Passwords do not match!');
-            return false;
-        }
-        return true;
-    }
-</script> -->
-<!-- 
-<!DOCTYPE html>
-<html>
+    
+?>
