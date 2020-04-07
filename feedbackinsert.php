a5d2fefd7fd113524ae09701a45920340bc94483
Anders <anderskoo@live.no>
2020-04-07 16:21:02 +0200

    kommentering

--- feedbackinsert.php
+++ feedbackinsert.php
@@ -29,6 +29,7 @@ Kodet og tilpasset av: Henrik Solnør Johansen og Vigleik Espeland Stakseng og
 // Setter opp kredentialer
 // ----------------------------------------------------------------------------------------------------
 
+//Kodet og tilpasset av: Anders Koo START
 $name = trim($_POST['name']); // Sjekker om variaber er deklarert og fjerner whitespace på start og slutt
 $email = trim($_POST['email']); // Sjekker om variaber er deklarert og fjerner whitespace på start og slutt
 $message = trim($_POST['message']); // Sjekker om variaber er deklarert og fjerner whitespace på start og slutt
@@ -40,7 +41,7 @@ $subject = trim($_POST['subject']); // Sjekker om variaber er deklarert og fjern
 // Forbereder insert spørring mot databasen med prepare
 // ----------------------------------------------------------------------------------------------------
 
-//Kodet og tilpasset av: Anders Koo
+
 $stmt = mysqli_prepare($mysqli,"INSERT INTO feedback(name, email, message, subject) VALUES(?,?,?,?)");
 
 // ----------------------------------------------------------------------------------------------------
@@ -49,7 +50,7 @@ $stmt = mysqli_prepare($mysqli,"INSERT INTO feedback(name, email, message, subje
 // mysqli_stmt_bind_param og mysqli_prepare metodene hører sammen og er en måte å sikre mot sql injection. 
 // ----------------------------------------------------------------------------------------------------
 
-//Kodet og tilpasset av: Anders Koo
+//Kodet og tilpasset av: Anders Koo STOPP
 mysqli_stmt_bind_param($stmt,"ssss", $name, $email, $message, $subject);
 if(mysqli_stmt_execute($stmt))
 {
