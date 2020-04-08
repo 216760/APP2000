240291752827bea0ef951a8c0ed580003d6d0213
Andreas Knutsen <31688396+216760@users.noreply.github.com>
2020-04-08 13:40:30 +0200

    update

--- includes/header.php
+++ includes/header.php
@@ -1,21 +1,19 @@
 <!-- ---------------------------------------------------------------------------------------------------- 
 Header
 ----------------------------------------------------------------------------------------------------- -->
-
 <head>
+  
+  <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Forside</title>
+  
+  <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="bootstrap-4.1.3-dist/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="css/stylesheet.css" id="lnk"/>
-  <!-- <link rel="stylesheet" href="css/stylefixed.css"> -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/stylesheet.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css" rel="stylesheet">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-</head>
 
+</head>
 <!-- ---------------------------------------------------------------------------------------------------- 
 Header
 ----------------------------------------------------------------------------------------------------- -->
