db448f41cfafa2d016abdd5fc3c8417b22138312
Andreas Knutsen <31688396+216760@users.noreply.github.com>
2020-04-08 14:01:20 +0200

    Update edit.php

--- edit.php
+++ edit.php
@@ -24,26 +24,27 @@ Setter opp session og includes
 
 <?php
 session_start();
-include('includes/header.php');
-include('includes/navbar.php'); 
+?>
+<!DOCTYPE html>
+<html>
 
+<?php
+include('includes/header.php');
 ?>
 
-<!-- ----------------------------------------------------------------------------------------------------
-Setter opp session og includes
------------------------------------------------------------------------------------------------------ -->
+    <body>
 
+<?php
+include('includes/navbar.php');
+?>
 
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<link rel="stylesheet" href="css/stylesheet.css">
-<link rel="stylesheet" href="css/Logginn.css">
-
-<div class="container-fluid">
-	<div class="card shadow mb-4 mx-auto" style="width: 30rem;">
-	  <div class="card-header py-3">
-	    <h6 class="m-0 font-weight-bold text-primary"> EDIT Subscription </h6>
-	  </div>
-	  <div class="card-body">
+        <div class="content-dashboard">
+            <div class="container">
+                <div class="card shadow mb-4 mx-auto" style="width: 30rem;">
+                    <div class="card-header py-3">
+                        <h6 class="m-0 font-weight-bold text-primary"> EDIT Subscription </h6>
+                    </div>
+                    <div class="card-body">
 <?php
 
 // ----------------------------------------------------------------------------------------------------
@@ -68,50 +69,61 @@ $connection = mysqli_connect("itfag.usn.no", "v20app2000u2", "pw2", "v20app2000d
 // Setter opp bootstrap struktur
 // ----------------------------------------------------------------------------------------------------
 
-        foreach($query_run as $row) // foreach brukes til telle antall rader i databasen som samsvarer med innlogget bruker
-        {
-            ?>
-    
-            <form action="server.php" method="POST">
-    
-            <input type="hidden" name="edit_id" value="<?php echo $row['id'] ?>">
-            <div class="form-group">
-                    <label> Beskrivelse </label>          
-                                                              <!-- Henter beskrivelse fra DB -->
-                    <input type="text" name="edit_description" value="<?php echo $row['description'] ?>" class="form-control" placeholder="Enter Username"> 
-                </div>
-                <div class="form-group">
-                    <label>Fra dato</label>                   <!-- Henter start dato fra DB -->
-                    <input type="date" name="edit_date_from" value="<?php echo $row['start_date'] ?>" class="form-control" placeholder="Enter Email">
-                </div>
-                <div class="form-group">
-                    <label>Til dato</label>                   <!-- Henter slutt dato fra DB -->
-                    <input type="date" name="edit_date_to" value="<?php echo $row['end_date'] ?>" class="form-control" placeholder="Enter Password">
-                </div> 
-                    <a href="dashboard.php" class="btn"> Cancel</a>
-                    <button type="submit" name="updatebtn" class="btn">Update</button>
-                </form>
-            </div>
-            </div>
-            </div>
-    
-            <?php
-        }
+    foreach($query_run as $row) // foreach brukes til telle antall rader i databasen som samsvarer med innlogget bruker
+    {
+    ?>
+
+                        <form action="server.php" method="POST">
+
+                            <input type="hidden" name="edit_id" value="<?php echo $row['id'] ?>">
+                            <div class="form-group">
+                                <label> Beskrivelse </label>          
+                                <!-- Henter data fra DB -->
+                                <input type="text" name="edit_description" value="<?php echo $row['description'] ?>" class="form-control" placeholder="Enter Username"> 
+                            </div>
+                            <div class="form-group">
+                                <label>Fra dato</label>                   <!-- Henter data fra DB -->
+                                <input type="date" name="edit_date_from" value="<?php echo $row['start_date'] ?>" class="form-control" placeholder="Enter Email">
+                            </div>
+                            <div class="form-group">
+                                <label>Til dato</label>                   <!-- Henter data fra DB -->
+                                <input type="date" name="edit_date_to" value="<?php echo $row['end_date'] ?>" class="form-control" placeholder="Enter Password">
+                            </div> 
+                            <a href="dashboard.php" class="btn btn-primary"> Cancel</a>
+                            <button type="submit" name="updatebtn" class="btn btn-primary">Update</button>
+                        </form>
+                    </div>
+
+    <?php
+    }
     }
     ?>
 
+                </div>
+            </div>
+        </div>
 
 
 
-<!-- --------------------------------------------------------------------------------------------------
- Footer 
------------------------------------------------------------------------------------------------------->
 
-<?php
-include('includes/footer.php');
-include('includes/scripts.php'); 
-?>
+    <!-- --------------------------------------------------------------------------------------------------
+    Footer 
+    ------------------------------------------------------------------------------------------------------>
 
-<!-- --------------------------------------------------------------------------------------------------
- Footer 
------------------------------------------------------------------------------------------------------->
+    <?php include('includes/footer.php');?>
+
+    <!-- --------------------------------------------------------------------------------------------------
+    Footer 
+    ------------------------------------------------------------------------------------------------------>
+
+
+    </body>
+
+<!-- ---------------------------------------------------------------------------------------------------- 
+Henter script filer (JS, JQUERY, BOOTSTRAP)
+----------------------------------------------------------------------------------------------------- -->
+<?php include'includes/scripts.php';?>
+<!-- ---------------------------------------------------------------------------------------------------- 
+Henter script filer (JS, JQUERY, BOOTSTRAP)
+----------------------------------------------------------------------------------------------------- -->
+<html>
