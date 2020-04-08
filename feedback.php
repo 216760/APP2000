264132f685cfd31abd51f45e745a02f9413c846a
Andreas Knutsen <31688396+216760@users.noreply.github.com>
2020-04-08 14:35:07 +0200

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -8,7 +8,7 @@ Etter dette gjør vi en vurdering om å bruke, tilpasse og implementer eksempele
 
 Kilde: http://www.openjs.com/ajax/tutorial/ajax.php
 
-Medlemmer som har birdratt Henrik Solnør Johansen, Vigleik Espeland Stakseng, Anders Koo og Andreas Knutsen
+Medlemmer som har bidratt Henrik Solnør Johansen, Vigleik Espeland Stakseng, Anders Koo og Andreas Knutsen
 
 ------------------------------------------------------------------------------------------------------------------------------------------------> 
 <!DOCTYPE html>
