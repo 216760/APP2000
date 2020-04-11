d73baf5a267fd5eaad24089ebe1f519bac22335e
GitMassy <47035843+GitMassy@users.noreply.github.com>
2020-04-11 12:32:37 +0200

    Update toggleDark.js

--- js/toggleDark.js
+++ js/toggleDark.js
@@ -5,15 +5,16 @@ function toggleDark(cssFile) {
     //-----------------------------------------------------------------------------------------------------------------------
     // Kodet av: Vigleik Espeland Stakseng
 
-    var checkBox = document.getElementById("toggle1"); // Finner selve checkboxen
+    var checkBox = document.getElementById("toggle1");      // Finner selve checkboxen
   
-    var link = document.getElementById("text");        // Finner output teksten
+    var link = document.getElementById("text");             // Finner output teksten
 
-    if (checkBox.checked == true){                     // if-setning som kjører dersom checkbox/toggleswitch er TRUE
-        var link = document.getElementById("lnk");       // Finner CSS filen ved hjelp av getElementById
-	    link.setAttribute("href", "css/sheetDark.css");    // Setter href til stylesheet for darkmode
-    } else {                                           // if-setning som kjører dersom checkbox/toggleswitch er FALSE
-        var link = document.getElementById("lnk");       // Finner CSS filen ved hjelp av getElementById
-	    link.setAttribute("href", "css/stylesheet.css");   // Setter href til stylesheet for vanlig nettside design (ikke darkmode)
+    if (checkBox.checked == true){                          // if-setning som kjører dersom checkbox/toggleswitch er TRUE
+        var link = document.getElementById("lnk");          // Finner CSS filen ved hjelp av getElementById
+        link.setAttribute("href", "css/sheetDark.css");     // Setter href til stylesheet for darkmode
+    } else {                                                // if-setning som kjører dersom checkbox/toggleswitch er FALSE
+        var link = document.getElementById("lnk");          // Finner CSS filen ved hjelp av getElementById
+        link.setAttribute("href", "css/sheetDark.css");     // Setter href til stylesheet for vanlig nettside design (ikke darkmode)
     }
+
 }
