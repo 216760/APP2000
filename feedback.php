df39b854f96c6df48184c394435735139aa449bc
Anders <anderskoo@live.no>
2020-04-16 12:13:28 +0200

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -2,7 +2,7 @@
 /*****************************************************************************************************************
 * TIL INFORMASJON:																							 	 *																		  
 * 																												 *
-* I denne filen ligger det gjenbrukt og tilpasset kode som er funnet på linkene oppsummert under.				 *
+* I denne filen ligger det tilpasset kode som er funnet på linkene oppsummert under.				 			 *	
 * Dette er også dokumentert under kildebruk i rapporten og markert i selve koden. 							 	 *
 * Grunnen til dette er basert på “best practice”  måter å programmere på.  										 *
 * Vi har gjennom en rekke eksempler lært oss hvordan php språket fungerer. 										 *
