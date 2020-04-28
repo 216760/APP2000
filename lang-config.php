7e702c25bde07501fee9a371d4e3b034297e61fd
Andreas <andreas_k83@hotmail.com>
2020-04-28 15:42:49 +0200

    Update lang-config.php

--- lang-config.php
+++ lang-config.php
@@ -28,12 +28,12 @@
     if (!isset($_SESSION['lang']))
         $_SESSION['lang'] = "en";
     // Sjekker at $_SESSION['lang'] IKKE er samme $_SESSION['lang'] som allerede har blitt deklarert
-    else if (isset($_GET['lang']) && $_SESSION['lang'] != $_GET['lang'] && !empty($_GET['lang'])) {
+    elseif (isset($_GET['lang']) && $_SESSION['lang'] != $_GET['lang'] && !empty($_GET['lang'])) {
         // Henter verdi til $_SESSION['lang'], og setter den til samme verdi som $_GET-funksjonen returnerer
         if ($_GET['lang'] == "en")
             $_SESSION['lang'] = "en";
         // Henter verdi til $_SESSION['lang'], og setter den til samme verdi som $_GET-funksjonen returnerer
-        else if ($_GET['lang'] == "no")
+        elseif ($_GET['lang'] == "no")
             $_SESSION['lang'] = "no";
     }
 
