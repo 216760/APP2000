c7b36a9fdaf1fdc3c434d09cc5bae117b7282a45
Anders <anderskoo@live.no>
2020-04-30 10:29:49 +0200

    Update feedbackinsert.php

--- feedbackinsert.php
+++ feedbackinsert.php
@@ -11,7 +11,8 @@
 * Kilde:                                                                                                         *
 *   https://www.youtube.com/playlist?list=PLk7v1Z2rk4hiJD24gvXHxzkfA2twWvxXV                                     * 
 *   https://www.youtube.com/watch?v=XV9x17zVZFU&list=PLk7v1Z2rk4hiJD24gvXHxzkfA2twWvxXV&index=3&t=0s             *
-*   https://websitebeaver.com/prepared-statements-in-php-mysqli-to-prevent-sql-injection                         *
+*   https://websitebeaver.com/prepared-statements-in-php-mysqli-to-prevent-sql-injection                         * 
+*   https://stackoverflow.com/questions/35931377/get-id-for-a-specific-user-in-mysql-database-in-php             *
 *                                                                                                                *
 * Vi har med vilje laget informasjonsboksene i koden med ulike størrelser 									     * 
 * for å kunne skille dem fra hverandre.                                                                          *                                                                                       
