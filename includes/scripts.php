ce36b24878632ad2c2920750b4812fda108b9f7b
Anders Koo Furulund <anderskoo@live.no>
2021-05-22 14:16:16 +0200

    Update scripts.php

--- includes/scripts.php
+++ includes/scripts.php
@@ -1,6 +1,6 @@
-<!-- ---------------------------------------------------------------------------------------------------- 
-Henter script filer (JS, JQUERY, BOOTSTRAP)
------------------------------------------------------------------------------------------------------ -->
+// ---------------------------------------------------------------------------------------------------- //  
+// Henter script filer (JS, JQUERY, BOOTSTRAP)                                                          //
+// ---------------------------------------------------------------------------------------------------- //
 
 <script src="js/jquery-3.3.1.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -9,6 +9,6 @@ Henter script filer (JS, JQUERY, BOOTSTRAP)
 <script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/js/bootstrap4-toggle.min.js"></script>
 <script src="js/toggleDark.js"></script>
 
-<!-- ---------------------------------------------------------------------------------------------------- 
-Henter script filer (JS, JQUERY, BOOTSTRAP)
------------------------------------------------------------------------------------------------------ -->
+// ---------------------------------------------------------------------------------------------------- //  
+// Henter script filer (JS, JQUERY, BOOTSTRAP)                                                          //
+// ---------------------------------------------------------------------------------------------------- //
