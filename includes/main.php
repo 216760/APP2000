971d2432242ecda5fc2718fc4e21d4a003e0f057
Anders Koo Furulund <anderskoo@live.no>
2021-05-22 14:10:54 +0200

    Update main.php

--- includes/main.php
+++ includes/main.php
@@ -1,16 +1,16 @@
 <!-- ---------------------------------------------------------------------------------------------------------- -->
-<!-- TIL INFORMASON:																							-->
-<!-- I denne filen ligger det kode som er hentet og tilpasset egen løsning fra kildene nedenfor.		        -->
-<!-- Dette er også dokumentert under "Kommentarer til kildebruk" i rapporten og markert i selve koden		    -->
-<!-- Grunnen til dette er basert på “best practice”  måter å programmere på.  							        -->
-<!-- Vi har gjennom en rekke eksempler via dokumenterte kilder lært oss hvordan php språket fungerer. 			-->
-<!-- 																											-->
-<!--  Kodet og tilpasset av: Henrik Solnør Johansen Andreas Knutsen og Anders Koo  								-->
-<!--										                      												-->
-<!--  Kilde: 																									-->                                       
-<!--    	https://www.youtube.com/watch?v=cgvDMUrQ3vA															-->
-<!-- 		https://www.w3schools.com/TAgs/tag_hn.asp															-->
-<!--                                                         													-->
+<!-- TIL INFORMASON:												-->
+<!-- I denne filen ligger det kode som er hentet og tilpasset egen løsning fra kildene nedenfor.		-->
+<!-- Dette er også dokumentert under "Kommentarer til kildebruk" i rapporten og markert i selve koden		-->
+<!-- Grunnen til dette er basert på “best practice”  måter å programmere på. 					-->
+<!-- Vi har gjennom en rekke eksempler via dokumenterte kilder lært oss hvordan php språket fungerer. 		-->
+<!-- 														-->
+<!--  Kodet og tilpasset av: Henrik Solnør Johansen Andreas Knutsen og Anders Koo  			 	-->
+<!--										                      		-->
+<!--  Kilde: 													-->                                       
+<!--    	https://www.youtube.com/watch?v=cgvDMUrQ3vA						        -->
+<!-- 		https://www.w3schools.com/TAgs/tag_hn.asp							-->
+<!--                                                         							-->
 <!-- ---------------------------------------------------------------------------------------------------------- -->
 
 <?php
@@ -21,9 +21,9 @@ include('lang-config.php'); // Inkluderer oppsett for flere språk
 
 	<div class="container-fluid">
 <!-- -------------------------------------------------------------------------------------------------------------------- -->
-<!-- START 																								  				  -->
-<!-- For å sette opp muligheter for både norsk og engelsk opppset av vi hentet kode og tilpasset fra kilden under  		  -->																	 
-<!-- Kilde: https://www.youtube.com/watch?v=OePNkDd3Yb8 												  				  -->
+<!-- START 														  -->
+<!-- For å sette opp muligheter for både norsk og engelsk opppset av vi hentet kode og tilpasset fra kilden under  	  -->																	 
+<!-- Kilde: https://www.youtube.com/watch?v=OePNkDd3Yb8 								  -->
 <!-- -------------------------------------------------------------------------------------------------------------------- -->
 
 		<h2 style="text-align:right">
@@ -44,9 +44,9 @@ include('lang-config.php'); // Inkluderer oppsett for flere språk
 		
 		</h2>
 <!-- -------------------------------------------------------------------------------------------------------------------- -->
-<!-- STOPP 																								  				  -->
-<!-- For å sette opp muligheter for både norsk og engelsk opppset av vi hentet kode og tilpasset fra kilden under  		  -->																	 
-<!-- Kilde: https://www.youtube.com/watch?v=OePNkDd3Yb8 												  				  -->
+<!-- STOPP 														  -->
+<!-- For å sette opp muligheter for både norsk og engelsk opppset av vi hentet kode og tilpasset fra kilden under         -->																	 
+<!-- Kilde: https://www.youtube.com/watch?v=OePNkDd3Yb8 								  -->
 <!-- -------------------------------------------------------------------------------------------------------------------- -->
 	</div>
 	
